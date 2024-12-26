43c1b52eb52fc72b42e2e35b1672152eb4c02371
NoorN <NoorN@NOOR>
2024-12-26 14:25:05 +0300

    Add project files.

---
+++ thursday/Models/Product.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thursday.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool InStock { get; set; }
+
+        public bool Stock { get; set; }
+
+
+
+        static List<Product> productsList = new List<Product>();
+        public void AddProduct (string name , double price, int quantity, bool stock, int counntIn, int countOut )
+        {
+            productsList.Add(new Product { Name = name, Price = price, Quantity = quantity, Stock = stock });
+
+        }
+
+        //public static bool CheckStock(int quantity)
+        //{
+           // if (quantity <= 0)
+            //    return false;
+           // else
+             //   return true;
+       // }
+        
+      //  public  string InStock()
+        //{
+           // if (IsInStock == 0)
+            
+       // }
+
+
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine(" -----all products -----");
+            foreach (Product iteam in productsList) {
+                int countIn = 0;
+                int countOut = 0;
+
+                if( iteam.Stock == true )
+                {
+                    countIn++;
+                    Console.WriteLine("Name " + iteam.Name + "      " + " Price " + iteam.Price + "kd    " + " Quantity" + iteam.Quantity + "  total  "
+                    +(iteam.Price, iteam.Quantity) + "in stock " + countIn);
+                }
+
+                else
+                {
+                    countOut++;
+                    Console.WriteLine("Name " + iteam.Name + "      " + " Price " + iteam.Price + "kd    " + " Quantity" + iteam.Quantity + " total  "
+                    +(iteam.Price, iteam.Quantity) + "out of stock " + countOut);
+                }
+            }
+        }
+
+        //1
+        public string GetInfo(string name)
+        {
+
+            return $"Product Name: {name}";
+        }
+
+        public string GetPrice(string name, double price)
+        {
+            return $"Product: {name}, Price: {price}";
+        }
+
+        public string GetDisplay(string name, double price, int quantity)
+        {
+            double total = price * quantity;
+            return $"Product: {name}, Price: {price}, Quantity: {quantity}, Total: {total} KD" ;
+        }
+
+
+        public int Calc (int price, int quantity)
+        {
+            return price * quantity;
+        }
+
+       
+
+        public string GetProduct()
+        { 
+            int countIn = 0;
+        int countOut = 0;
+
+        
+            foreach( Product iteam in productsList)
+            {
+
+                if (iteam.InStock == true)
+                {
+                    countIn++;
+                }
+                else
+                {
+                    countOut++;
+                }
+                    Console.WriteLine("Name is :" + iteam.Name + " the price is :" + iteam.Price + " the quantity is :" + iteam.Quantity + " total : " + Calc((int)iteam.Price, iteam.Quantity) + "out of stock" + countOut);
+            }
+            return "in stock" + countIn + "out of stock" + countOut;
+        }
+
+    }
+}
+
---
+++ thursday/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System.Diagnostics.Tracing;
+using thursday.Models;
+
+Product product = new Product();
+Product p = new Product();
+int Count;
+string NAME;
+double PRICE;
+int QTY;
+int countIn = 0;
+int countOut = 0;
+bool stock=false;
+Console.WriteLine("enter how many products you have please !");
+Count = Convert.ToInt32(Console.ReadLine());
+
+// this will only accepts one product name !!
+//Console.WriteLine("Enter product name:");
+//string name = Console.ReadLine();
+//Console.WriteLine(product.GetInfo(name));
+
+
+// acc only product name and price 
+
+//Console.WriteLine("Enter product name:");
+// name = Console.ReadLine();
+//Console.WriteLine("Enter product price:");
+//double price = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine(product.GetPrice(name, price));
+
+
+// ac product name , price quantity 
+
+//Console.WriteLine("Enter product name:");
+//name = Console.ReadLine();
+//Console.WriteLine("Enter product price:");
+//price = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Enter product quantity:");
+//int quantity = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(product.GetDisplay(name, price, quantity));
+
+
+for (int i = 1; i <= Count; i++)
+{
+    Console.WriteLine("enter the name of the product ");
+    NAME = Console.ReadLine();
+    Console.WriteLine(" please enter the price :");
+    PRICE = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("enter the quantity of the products ");
+    QTY = Convert.ToInt32(Console.ReadLine());
+
+    // for  ( int  x = 1; x<= QTY; x++ )
+
+    Console.Write("enter avaliblity for the iteam");
+    stock = Convert.ToBoolean(Console.ReadLine());
+    p.AddProduct(NAME, PRICE, QTY, stock, countIn, countOut);
+
+
+
+
+
+}
+
+    p.DisplayProducts();
+    p.GetProduct();
+Console.WriteLine(p.GetProduct);
